6af7c9d2996a8cec10dfc6b269ac1770413aa654
fhayvy <ezefavourchiamaka01@gmail.com>
2024-10-02 16:16:23 +0100

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""Module contains pascal_triangle fnction"""
+"""This script produces a Pascal's Triangle"""
 
 
-def pascal_triangle(n: int) -> list:
-    """
-    Args:
-      n: integer
-    return:
-      a list of list of pascal triangle
-    """
-    if n <= 0:
-        return []
-    pascal = []
+def pascal_triangle(n):
+	if n <= 0:
+		return []
+	
+	result = []
 
-    for i in range(0, n):
-        pascal.append([comb(i, k) for k in range(0, i + 1)])
+	for row in range(1, n):
+		new_row = [1]
 
-    return pascal
+		for i in range(1, row):
+			new_row.append(result[-1][i-1] + result[-1][i])
 
+		new_row.append(1)
+		result.append(new_row)
 
-def comb(n, k):
-    """return the combination of n,k"""
-    c = fact(n) // (fact(n-k) * fact(k))
-    return c
+	return result
 
 
-def fact(n):
-    """return n factorial"""
-    if n <= 1:
-        return 1
-    return n * fact(n-1)
