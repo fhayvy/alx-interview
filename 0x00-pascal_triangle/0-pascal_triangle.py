272462f8d0853360ca5e843b9ae5e3c263e8aab9
fhayvy <ezefavourchiamaka01@gmail.com>
2024-10-02 15:57:36 +0100

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,19 @@
-jj
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+	if n <= 0:
+		return []
+	
+	result = [[1]]
+
+	for row in range(1, n):
+		new_row = [1]
+
+		for i in range(1, row):
+			new_row.append(result[-1][i-1] + result[-1][i])
+
+		new_row.append(1)
+		result.append(new_row)
+
+	return result
